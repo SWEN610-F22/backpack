9feb22197ea052bcb0b46e77987945d49020c1a9
Sam Singh Anantha <ss8650@g.rit.edu>
2022-09-29 19:34:46 -0400

    create getAll products endpoint

---
+++ estore-api/src/main/java/com/estore/api/estoreapi/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.estore.api.estoreapi.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estore.api.estoreapi.model.Product;
+import com.estore.api.estoreapi.persistence.ProductDAO;
+
+@RestController
+@RequestMapping("products")
+public class ProductController {
+    private static final Logger LOG = Logger.getLogger(ProductController.class.getName());
+    private ProductDAO productDao;
+
+    public ProductController(ProductDAO productDAO){
+        this.productDao = productDAO;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Product[]> getAllProducts(){
+        try{
+            Product[] products = productDao.getProducts();
+            return new ResponseEntity<Product[]>(products, HttpStatus.OK);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        
+    }
+}
