6ed132e243fb150aab8c7bf0c11eea2c69ad3675
Princess Regina Afu <pa3578@rit.edu>
2022-10-03 14:17:11 -0400

    create products added

--- estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/persistence/ProductDAO.java
@@ -6,4 +6,14 @@ import com.estore.api.estoreapi.model.Product;
 
 public interface ProductDAO {
     Product[] getProducts() throws IOException;
+
+    /**
+     * Creates and saves a {@linkplain Product product}
+     * @param product {@linkplain Product product} object to be created and saved.
+     * The id of the product object is ignored and a new unique id is assigned.
+     * @return new {@link Product product} if successful, false otherwise.
+     * @throws IOException if there is an issue with underlying storage.
+     */
+    Product createProduct(Product product) throws IOException;
 }
+
