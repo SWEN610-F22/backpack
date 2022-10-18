30592bd130f3367bc817a6d466152341cd5f99d1
Utkarsh Sharma <utkarshsharma211@gmail.com>
2022-10-18 19:35:18 -0400

    committing and pushing test cases for Product

--- estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/model/ProductTest.java
@@ -1,11 +1,13 @@
 package com.estore.api.estoreapi.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import net.bytebuddy.build.ToStringPlugin;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("Model-tier")
 public class ProductTest {
-
+    Product product = new Product(14,"Hiking Stick","Can be used for hiking", 20.0, 5);
     @Test
     void createProduct(){
         Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
@@ -15,5 +17,36 @@ public class ProductTest {
         assertEquals(35.0, product.getPrice());
         assertEquals(10, product.getQuantity());
     }
-    
+
+    @Test
+    void setId() {
+        product.setId(15);
+        assertEquals(15, product.getId());
+    }
+    @Test
+    void setName(){
+        product.setName("something else");
+        assertEquals("something else", product.getName());
+    }
+    @Test
+    void setPrice(){
+        product.setPrice(25.67);
+        assertEquals(25.67, product.getPrice());
+    }
+    @Test
+    void setQuantity() {
+        product.setQuantity(2);
+        assertEquals(2, product.getQuantity());
+    }
+    @Test
+    void setDescription(){
+        product.setDescription("It doesn't matter");
+        assertEquals("It doesn't matter", product.getDescription());
+    }
+
+    @Test
+    public void testToString(){
+        assertEquals("Product [id=" + product.getId() +", name=" + product.getName() + ", description=" + product.getDescription() + ", price=" + product.getPrice() + ", quantity=" + product.getQuantity() + "]", product.toString());
+    }
+
 }
