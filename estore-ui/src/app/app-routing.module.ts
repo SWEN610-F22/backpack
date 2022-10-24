20e581e3dc2311f7aa7c9238261405134a8e4637
Sam Singh Anantha <ss8650@g.rit.edu>
2022-10-24 14:23:58 -0400

    fix(router): landing

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [{path: '/', component: LandingComponent}];
+const routes: Routes = [{path: '', component: LandingComponent}];
 
 
 @NgModule({
