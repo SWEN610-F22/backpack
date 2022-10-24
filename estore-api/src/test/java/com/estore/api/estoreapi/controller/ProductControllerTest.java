c7cafd34cda782353d99ca12c8dde9f036abdb60
Sam Singh Anantha <ss8650@g.rit.edu>
2022-10-24 15:37:45 -0400

    update product to include manufacturer and image url

--- estore-api/src/test/java/com/estore/api/estoreapi/controller/ProductControllerTest.java
+++ estore-api/src/test/java/com/estore/api/estoreapi/controller/ProductControllerTest.java
@@ -33,7 +33,7 @@ public class ProductControllerTest {
 
     @Test
     public void getProduct() throws IOException {
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10, "fish", "http://www.google.com");
     
         when(mockProductDAO.getProduct(product.getId())).thenReturn(product);
         ResponseEntity<Product> response = productController.getProduct(product.getId());
@@ -59,7 +59,7 @@ public class ProductControllerTest {
 
     @Test
     public void createProductSuccessfully() throws IOException {
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         when(mockProductDAO.createProduct(product)).thenReturn(product);
         Product[] matching = new Product[0];
         when(mockProductDAO.findProducts("Fishing rod")).thenReturn(matching);
@@ -70,7 +70,7 @@ public class ProductControllerTest {
 
     @Test
     public void createProductFail() throws IOException {
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         when(mockProductDAO.createProduct(product)).thenReturn(null);
         Product[] matching = {product};
         when(mockProductDAO.findProducts("Fishing rod")).thenReturn(matching);
@@ -80,7 +80,7 @@ public class ProductControllerTest {
 
     @Test
     public void createProductWithException() throws IOException { 
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         doThrow(new IOException()).when(mockProductDAO).createProduct(product);
         Product[] matching = new Product[0];
         when(mockProductDAO.findProducts("Fishing rod")).thenReturn(matching);
@@ -90,7 +90,7 @@ public class ProductControllerTest {
 
     @Test
     public void updateProductSuccessfully() throws IOException {
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         when(mockProductDAO.updateProduct(product)).thenReturn(product);
         ResponseEntity<Product> response = productController.updateProduct(product);
         product.setName("New Fishing Rod");
@@ -101,7 +101,7 @@ public class ProductControllerTest {
 
     @Test
     public void updateProductFailed() throws IOException {
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         when(mockProductDAO.updateProduct(product)).thenReturn(null);
         ResponseEntity<Product> response = productController.updateProduct(product);
         assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
@@ -109,7 +109,7 @@ public class ProductControllerTest {
 
     @Test
     public void updateProductHandleException() throws IOException {
-        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
+        Product product = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         doThrow(new IOException()).when(mockProductDAO).updateProduct(product);
         ResponseEntity<Product> response = productController.updateProduct(product);
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
@@ -118,8 +118,8 @@ public class ProductControllerTest {
     @Test
     public void getProducts() throws IOException {
         Product[] products = new Product[2];
-        products[0] = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
-        products[1] = new Product(2, "Fishing rod 2", "Can be used for fishing", 35.0, 10);
+        products[0] = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
+        products[1] = new Product(2, "Fishing rod 2", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
         when(mockProductDAO.getProducts()).thenReturn(products);
         ResponseEntity<Product[]> response = productController.getProducts(null);
         assertEquals(HttpStatus.OK,response.getStatusCode());
@@ -137,9 +137,9 @@ public class ProductControllerTest {
     public void getProductsWithSearch() throws IOException {
         String searchString = "camp";
         Product[] products = new Product[3];
-        products[0] = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10);
-        products[1] = new Product(2, "Camping gear", "Can be used for gear", 5.0, 2);
-        products[2] = new Product(3, "Hiking boots", "Can be used for boots", 13.0, 5);
+        products[0] = new Product(1, "Fishing rod", "Can be used for fishing", 35.0, 10,"fish", "http://www.google.com");
+        products[1] = new Product(2, "Camping gear", "Can be used for gear", 5.0, 2,"fish", "http://www.google.com");
+        products[2] = new Product(3, "Hiking boots", "Can be used for boots", 13.0, 5,"fish", "http://www.google.com");
         Product[] searched = {products[1]};
         when(mockProductDAO.findProducts(searchString)).thenReturn(searched);
         ResponseEntity<Product[]> response = productController.getProducts(searchString);
