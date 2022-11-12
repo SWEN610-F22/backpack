3ea3596c0c14c00267eda1bde96853bb8530e302
Princess Regina Afu <pa3578@rit.edu>
2022-11-12 17:27:44 -0500

    UI for detailed view done

--- estore-ui/src/app/pages/backpack-detail/backpack-detail.component.ts
+++ estore-ui/src/app/pages/backpack-detail/backpack-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-backpack-detail',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackpackDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
   }
 
 }
