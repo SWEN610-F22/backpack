0e3ec1c9febe1a8dd36a2463664e40c56082726e
Sam Singh Anantha <ss8650@g.rit.edu>
2022-11-14 13:52:39 -0500

    fix(ui): view all backpacks

--- estore-ui/src/app/components/view-backpacks/view-backpacks.component.ts
+++ estore-ui/src/app/components/view-backpacks/view-backpacks.component.ts
@@ -17,12 +17,16 @@ export class ViewBackpacksComponent implements OnInit {
 
   updateImages(): void{
     for(let i=0; i<this.backpacks.length; i++){
-      if(this.backpacks[i].activity=="fishing"){
-        this.backpacks[i].imageURL="https://freshwatervacationrentals.com/wp-content/uploads/2020/06/fishing.jpg";
+      if(this.backpacks[i].activity=="fishing" || this.backpacks[i].activity=="Fishing"){
+        this.backpacks[i].imageURL="https://images.unsplash.com/photo-1638919800429-e3a11ea32321?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80";
       }
-      else if(this.backpacks[i].activity=="hiking"){
+      else if(this.backpacks[i].activity=="hiking" || this.backpacks[i].activity=="Hiking"){
         this.backpacks[i].imageURL="https://images.unsplash.com/photo-1551632811-561732d1e306?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aGlraW5nfGVufDB8fDB8fA%3D%3D&w=1000&q=80";
       }
+      else if(this.backpacks[i].activity=="camping" || this.backpacks[i].activity=="Camping"){
+        this.backpacks[i].imageURL="https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
+      }
+      
     }
   }
 
